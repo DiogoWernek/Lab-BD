d2227ac920102d6a2b257714b40221c1f52cc52e
DiogoWernek <diogowernek86@gmail.com>
2025-04-29 19:48:37 -0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { MyRoutes } from "./routes";
+
 export function App() {
   return (
-      <div>
-        <h1 className="text-xl text-red-50">Página Principal</h1>
-      </div>
+    <MyRoutes />
   )
 }
